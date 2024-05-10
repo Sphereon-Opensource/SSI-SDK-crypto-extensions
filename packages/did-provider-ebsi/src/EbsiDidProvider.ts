38a486f41bb6880ca2cd37c85889275a267632bd
Maikel Maas <mmaas@sphereon.com>
2024-05-10 14:47:54 +0200

    refactor: moved Veramo unrelated methods to functions.ts

--- packages/did-provider-ebsi/src/EbsiDidProvider.ts
+++ packages/did-provider-ebsi/src/EbsiDidProvider.ts
@@ -1,36 +1,9 @@
-import {IAgentContext, IDIDManager, IIdentifier, IKeyManager, MinimalImportableKey} from '@veramo/core'
+import { IAgentContext, IDIDManager, IIdentifier, IKeyManager } from '@veramo/core'
 import Debug from 'debug'
-import {AbstractIdentifierProvider} from '@veramo/did-manager/build/abstract-identifier-provider'
-import {IKey, IService} from '@veramo/core/build/types/IIdentifier'
-import * as u8a from 'uint8arrays'
-import {
-  ApiOpts,
-  baseDocument,
-  CreateEbsiDidParams,
-  ebsiDIDSpecInfo,
-  EbsiKeyType,
-  EbsiPublicKeyPurpose,
-  IContext,
-  ICreateIdentifierArgs,
-  IKeyOpts,
-  Response,
-  Response200,
-  UpdateIdentifierParams,
-} from './types'
-import {
-  calculateJwkThumbprint,
-  formatEbsiPublicKey,
-  generateEbsiPrivateKeyHex,
-  generateMethodSpecificId
-} from './functions'
-import {
-  addVerificationMethod,
-  addVerificationMethodRelationship,
-  insertDidDocument,
-  sendSignedTransaction,
-} from './services/EbsiRPCService'
-import {toJwk} from '@sphereon/ssi-sdk-ext.key-utils'
-import {Transaction} from 'ethers'
+import { AbstractIdentifierProvider } from '@veramo/did-manager/build/abstract-identifier-provider'
+import { IKey, IService } from '@veramo/core/build/types/IIdentifier'
+import { ApiOpts, ebsiDIDSpecInfo, IContext, ICreateIdentifierArgs, UpdateIdentifierParams } from './types'
+import { createEbsiDid, generateEbsiKeyPair, generateMethodSpecificId } from './functions'
 
 const debug = Debug('sphereon:did-provider-ebsi')
 
@@ -48,19 +21,19 @@ export class EbsiDidProvider extends AbstractIdentifierProvider {
     const { type, options, kms, alias } = { ...args }
 
     if (!type || type === ebsiDIDSpecInfo.V1) {
-      const secp256k1ManagedKeyInfo = await this.generateEbsiKeyPair(
+      const secp256k1ManagedKeyInfo = await generateEbsiKeyPair(
         {
           keyOpts: options?.secp256k1Key,
           keyType: 'Secp256k1',
-          kms,
+          kms: kms ?? this.defaultKms,
         },
         context
       )
-      const secp256r1ManagedKeyInfo = await this.generateEbsiKeyPair(
+      const secp256r1ManagedKeyInfo = await generateEbsiKeyPair(
         {
           keyOpts: options?.secp256r1Key,
           keyType: 'Secp256r1',
-          kms,
+          kms: kms ?? this.defaultKms,
         },
         context
       )
@@ -79,7 +52,7 @@ export class EbsiDidProvider extends AbstractIdentifierProvider {
         throw new Error(`Options must be provided ${JSON.stringify(options)}`)
       }
 
-      await this.createEbsiDid(
+      await createEbsiDid(
         {
           identifier,
           secp256k1ManagedKeyInfo,
@@ -88,6 +61,7 @@ export class EbsiDidProvider extends AbstractIdentifierProvider {
           from: options.from,
           notBefore: options.notBefore,
           notAfter: options.notAfter,
+          apiOpts: options.apiOpts ?? this.apiOpts,
         },
         context
       )
@@ -98,132 +72,6 @@ export class EbsiDidProvider extends AbstractIdentifierProvider {
       throw new Error(`Type ${type} not supported. Please use @sphereon/ssi-sdk-ext.did-provider-key for Natural Person EBSI DIDs`)
     }
     throw new Error(`Type ${type} not supported`)
-  }
-
-  private async createEbsiDid(
-    args: CreateEbsiDidParams,
-
-    context: IContext
-  ): Promise<void> {
-    const insertDidDocTransaction = await insertDidDocument({
-      params: [
-        {
-          from: args.from,
-          did: args.identifier.did,
-          baseDocument:
-            args.baseDocument ?? baseDocument,
-          vMethoddId: await calculateJwkThumbprint({ jwk: toJwk(args.secp256k1ManagedKeyInfo.publicKeyHex, 'Secp256k1') }),
-          isSecp256k1: true,
-          publicKey: formatEbsiPublicKey({ key: args.secp256k1ManagedKeyInfo, type: 'Secp256k1' }),
-          notBefore: args.notBefore,
-          notAfter: args.notAfter,
-        },
-      ],
-      id: args.id,
-      apiOpts: args.apiOpts ?? this.apiOpts,
-      token: '', //TODO hook it up: https://sphereon.atlassian.net/browse/SDK-10
-    })
-
-    await this.sendTransaction(
-      { docTransactionResponse: insertDidDocTransaction, kid: args.secp256k1ManagedKeyInfo.kid, id: args.id, apiOpts: args.apiOpts },
-      context
-    )
-
-    const addVerificationMethodTransaction = await addVerificationMethod({
-      params: [
-        {
-          from: args.from,
-          did: args.identifier.did,
-          isSecp256k1: true,
-          vMethoddId: await calculateJwkThumbprint({ jwk: toJwk(args.secp256k1ManagedKeyInfo.publicKeyHex, 'Secp256k1') }),
-          publicKey: formatEbsiPublicKey({ key: args.secp256k1ManagedKeyInfo, type: 'Secp256k1' }),
-        },
-      ],
-      id: args.id,
-      apiOpts: args.apiOpts ?? this.apiOpts,
-      token: '', //TODO hook it up: https://sphereon.atlassian.net/browse/SDK-10
-    })
-
-    await this.sendTransaction(
-      { docTransactionResponse: addVerificationMethodTransaction, kid: args.secp256k1ManagedKeyInfo.kid, id: args.id, apiOpts: args.apiOpts },
-      context
-    )
-
-    const addVerificationMethodRelationshipTransaction = await addVerificationMethodRelationship({
-      params: [
-        {
-          from: args?.from,
-          did: args.identifier.did,
-          vMethoddId: await calculateJwkThumbprint({ jwk: toJwk(args.secp256r1ManagedKeyInfo.publicKeyHex, 'Secp256r1') }),
-          name: 'assertionMethod',
-          notAfter: 1,
-          notBefore: 1,
-        },
-      ],
-      id: args.id,
-      apiOpts: args.apiOpts ?? this.apiOpts,
-      token: '', //TODO hook it up: https://sphereon.atlassian.net/browse/SDK-10
-    })
-
-    await this.sendTransaction(
-      {
-        docTransactionResponse: addVerificationMethodRelationshipTransaction,
-        kid: args.secp256k1ManagedKeyInfo.kid,
-        id: args.id,
-        apiOpts: args.apiOpts,
-      },
-      context
-    )
-  }
-
-  private sendTransaction = async (args: { docTransactionResponse: Response; kid: string; id: number; apiOpts?: ApiOpts }, context: IContext) => {
-    if ('status' in args.docTransactionResponse) {
-      throw new Error(JSON.stringify(args.docTransactionResponse, null, 2))
-    }
-    const unsignedTransaction = (args.docTransactionResponse as Response200).result
-
-    const signedRawTransaction = await context.agent.keyManagerSignEthTX({
-      kid: args.kid,
-      transaction: unsignedTransaction,
-    })
-
-    const { r, s, v } = Transaction.from(signedRawTransaction).signature!
-
-    const sTResponse = await sendSignedTransaction({
-      params: [
-        {
-          protocol: 'eth',
-          unsignedTransaction: unsignedTransaction,
-          r,
-          s,
-          v: v.toString(),
-          signedRawTransaction,
-        },
-      ],
-      id: args.id,
-      apiOpts: args.apiOpts ?? this.apiOpts,
-      token: '', //TODO hook it up: https://sphereon.atlassian.net/browse/SDK-10
-    })
-
-    if ('status' in sTResponse) {
-      throw new Error(JSON.stringify(sTResponse, null, 2))
-    }
-  }
-
-  private async generateEbsiKeyPair(args: { keyOpts?: IKeyOpts; keyType: EbsiKeyType; kms?: string }, context: IAgentContext<IKeyManager>) {
-    const { keyOpts, keyType, kms } = args
-    let privateKeyHex = generateEbsiPrivateKeyHex(
-      ebsiDIDSpecInfo.V1,
-      keyOpts?.privateKeyHex ? u8a.fromString(keyOpts.privateKeyHex, 'base16') : undefined
-    )
-    if (privateKeyHex.startsWith('0x')) {
-      privateKeyHex = privateKeyHex.substring(2)
-    }
-    if (!privateKeyHex || privateKeyHex.length !== 64) {
-      throw new Error('Private key should be 32 bytes / 64 chars hex')
-    }
-    const importableKey = this.assertedKey({ key: { ...keyOpts, privateKeyHex }, type: keyType, kms })
-    return await context.agent.keyManagerImport(importableKey)
   }
 
   addKey(
@@ -277,71 +125,5 @@ export class EbsiDidProvider extends AbstractIdentifierProvider {
   // TODO How does it work? Not inferable from the api: https://hub.ebsi.eu/apis/pilot/did-registry/v5/post-jsonrpc#updatebasedocument
   async updateIdentifier(args: UpdateIdentifierParams, context: IAgentContext<IKeyManager & IDIDManager>): Promise<IIdentifier> {
     throw new Error(`Not (yet) implemented for the EBSI did provider`)
-  }
-
-  private assertedKey = (args: { key?: IKeyOpts; type: EbsiKeyType; kms?: string }): MinimalImportableKey => {
-    const { key, type, kms } = args
-    const minimalImportableKey: Partial<MinimalImportableKey> = { ...key } ?? {}
-    minimalImportableKey.kms = this.assertedKms(kms)
-    minimalImportableKey.type = this.setDefaultKeyType({ key, type })
-    minimalImportableKey.meta = { purposes: this.assertedPurposes({ key }) ?? this.setDefaultPurposes({ key, type }) }
-    return minimalImportableKey as MinimalImportableKey
-  }
-
-  private assertedKms(kms?: string) {
-    const result = kms ?? this.defaultKms
-    if (!!result) {
-      return result
-    }
-    throw new Error('no KMS supplied')
-  }
-
-  private setDefaultKeyType = (args: { key?: IKeyOpts; type: EbsiKeyType }): EbsiKeyType => {
-    if (!args.key?.type) {
-      return args.type
-    }
-    return args.key.type
-  }
-
-  private assertedPurposes = (args: { key?: IKeyOpts }): EbsiPublicKeyPurpose[] | undefined => {
-    const { key } = args
-    if (key?.purposes && key.purposes.length > 0) {
-      switch (key.type) {
-        case 'Secp256k1': {
-          if (key?.purposes && key.purposes.length > 0 && key.purposes?.includes(EbsiPublicKeyPurpose.CapabilityInvocation)) {
-            return key.purposes
-          }
-          throw new Error(`Secp256k1 key requires ${EbsiPublicKeyPurpose.CapabilityInvocation} purpose`)
-        }
-        case 'Secp256r1': {
-          if (
-            key?.purposes &&
-            key.purposes.length > 0 &&
-            key.purposes.every((purpose) => [EbsiPublicKeyPurpose.AssertionMethod, EbsiPublicKeyPurpose.Authentication].includes(purpose))
-          ) {
-            return key.purposes
-          }
-          throw new Error(`Secp256r1 key requires ${[EbsiPublicKeyPurpose.AssertionMethod, EbsiPublicKeyPurpose.Authentication].join(', ')} purposes`)
-        }
-        default:
-          throw new Error(`Unsupported key type: ${key.type}`)
-      }
-    }
-    return key?.purposes
-  }
-
-  private setDefaultPurposes = (args: { key?: IKeyOpts; type: EbsiKeyType }): EbsiPublicKeyPurpose[] => {
-    const { key, type } = args
-    if (!key?.purposes || key.purposes.length === 0) {
-      switch (type) {
-        case 'Secp256k1':
-          return [EbsiPublicKeyPurpose.CapabilityInvocation]
-        case 'Secp256r1':
-          return [EbsiPublicKeyPurpose.AssertionMethod, EbsiPublicKeyPurpose.Authentication]
-        default:
-          throw new Error(`Unsupported key type: ${key?.type}`)
-      }
-    }
-    return key.purposes
   }
 }
--- packages/did-provider-ebsi/src/functions.ts
+++ packages/did-provider-ebsi/src/functions.ts
@@ -1,10 +1,24 @@
-import { ebsiDIDSpecInfo, EbsiDidSpecInfo, EbsiEnvironment, EbsiKeyType } from './types'
+import {
+  ApiOpts,
+  baseDocument,
+  CreateEbsiDidParams,
+  ebsiDIDSpecInfo,
+  EbsiDidSpecInfo,
+  EbsiEnvironment,
+  EbsiKeyType,
+  EbsiPublicKeyPurpose,
+  IContext,
+  IKeyOpts,
+  Response,
+  Response200,
+} from './types'
 import { randomBytes } from '@ethersproject/random'
 import * as u8a from 'uint8arrays'
 import { base58btc } from 'multiformats/bases/base58'
-import { IKey } from '@veramo/core'
-import { getBytes, SigningKey, sha256, sha512 } from 'ethers'
-import { JwkKeyUse, toJwk, JWK } from '@sphereon/ssi-sdk-ext.key-utils'
+import { IAgentContext, IKey, IKeyManager, MinimalImportableKey } from '@veramo/core'
+import { getBytes, sha256, sha512, SigningKey, Transaction } from 'ethers'
+import { JWK, JwkKeyUse, toJwk } from '@sphereon/ssi-sdk-ext.key-utils'
+import { addVerificationMethod, addVerificationMethodRelationship, insertDidDocument, sendSignedTransaction } from './services/EbsiRPCService'
 
 export const base64url = (input: string): string => u8a.toString(u8a.fromString(input), 'base64url')
 
@@ -117,4 +131,191 @@ const checkPresent = (value: unknown, description: string) => {
   if (typeof value !== 'string' || !value) {
     throw new Error(`${description} missing or invalid`)
   }
+}
+
+export const sendTransaction = async (args: { docTransactionResponse: Response; kid: string; id: number; apiOpts?: ApiOpts }, context: IContext) => {
+  if ('status' in args.docTransactionResponse) {
+    throw new Error(JSON.stringify(args.docTransactionResponse, null, 2))
+  }
+  const unsignedTransaction = (args.docTransactionResponse as Response200).result
+
+  const signedRawTransaction = await context.agent.keyManagerSignEthTX({
+    kid: args.kid,
+    transaction: unsignedTransaction,
+  })
+
+  const { r, s, v } = Transaction.from(signedRawTransaction).signature!
+
+  const sTResponse = await sendSignedTransaction({
+    params: [
+      {
+        protocol: 'eth',
+        unsignedTransaction: unsignedTransaction,
+        r,
+        s,
+        v: v.toString(),
+        signedRawTransaction,
+      },
+    ],
+    id: args.id,
+    apiOpts: args.apiOpts,
+    token: '', //TODO hook it up: https://sphereon.atlassian.net/browse/SDK-10
+  })
+
+  if ('status' in sTResponse) {
+    throw new Error(JSON.stringify(sTResponse, null, 2))
+  }
+}
+
+export const generateEbsiKeyPair = async (args: { keyOpts?: IKeyOpts; keyType: EbsiKeyType; kms?: string }, context: IAgentContext<IKeyManager>) => {
+  const { keyOpts, keyType, kms } = args
+  let privateKeyHex = generateEbsiPrivateKeyHex(
+    ebsiDIDSpecInfo.V1,
+    keyOpts?.privateKeyHex ? u8a.fromString(keyOpts.privateKeyHex, 'base16') : undefined
+  )
+  if (privateKeyHex.startsWith('0x')) {
+    privateKeyHex = privateKeyHex.substring(2)
+  }
+  if (!privateKeyHex || privateKeyHex.length !== 64) {
+    throw new Error('Private key should be 32 bytes / 64 chars hex')
+  }
+  const importableKey = assertedKey({ key: { ...keyOpts, privateKeyHex }, type: keyType, kms })
+  return await context.agent.keyManagerImport(importableKey)
+}
+
+export const assertedKey = (args: { key?: IKeyOpts; type: EbsiKeyType; kms?: string }): MinimalImportableKey => {
+  const { key, type, kms } = args
+  const minimalImportableKey: Partial<MinimalImportableKey> = { ...key } ?? {}
+  minimalImportableKey.kms = assertedKms(kms)
+  minimalImportableKey.type = setDefaultKeyType({ key, type })
+  minimalImportableKey.meta = { purposes: assertedPurposes({ key }) ?? setDefaultPurposes({ key, type }) }
+  return minimalImportableKey as MinimalImportableKey
+}
+
+export const assertedKms = (kms?: string) => {
+  const result = kms
+  if (!!result) {
+    return result
+  }
+  throw new Error('no KMS supplied')
+}
+
+export const setDefaultKeyType = (args: { key?: IKeyOpts; type: EbsiKeyType }): EbsiKeyType => {
+  if (!args.key?.type) {
+    return args.type
+  }
+  return args.key.type
+}
+
+export const assertedPurposes = (args: { key?: IKeyOpts }): EbsiPublicKeyPurpose[] | undefined => {
+  const { key } = args
+  if (key?.purposes && key.purposes.length > 0) {
+    switch (key.type) {
+      case 'Secp256k1': {
+        if (key?.purposes && key.purposes.length > 0 && key.purposes?.includes(EbsiPublicKeyPurpose.CapabilityInvocation)) {
+          return key.purposes
+        }
+        throw new Error(`Secp256k1 key requires ${EbsiPublicKeyPurpose.CapabilityInvocation} purpose`)
+      }
+      case 'Secp256r1': {
+        if (
+          key?.purposes &&
+          key.purposes.length > 0 &&
+          key.purposes.every((purpose) => [EbsiPublicKeyPurpose.AssertionMethod, EbsiPublicKeyPurpose.Authentication].includes(purpose))
+        ) {
+          return key.purposes
+        }
+        throw new Error(`Secp256r1 key requires ${[EbsiPublicKeyPurpose.AssertionMethod, EbsiPublicKeyPurpose.Authentication].join(', ')} purposes`)
+      }
+      default:
+        throw new Error(`Unsupported key type: ${key.type}`)
+    }
+  }
+  return key?.purposes
+}
+
+export const setDefaultPurposes = (args: { key?: IKeyOpts; type: EbsiKeyType }): EbsiPublicKeyPurpose[] => {
+  const { key, type } = args
+  if (!key?.purposes || key.purposes.length === 0) {
+    switch (type) {
+      case 'Secp256k1':
+        return [EbsiPublicKeyPurpose.CapabilityInvocation]
+      case 'Secp256r1':
+        return [EbsiPublicKeyPurpose.AssertionMethod, EbsiPublicKeyPurpose.Authentication]
+      default:
+        throw new Error(`Unsupported key type: ${key?.type}`)
+    }
+  }
+  return key.purposes
+}
+
+export const createEbsiDid = async (args: CreateEbsiDidParams, context: IContext): Promise<void> => {
+  const insertDidDocTransaction = await insertDidDocument({
+    params: [
+      {
+        from: args.from,
+        did: args.identifier.did,
+        baseDocument: args.baseDocument ?? baseDocument,
+        vMethoddId: await calculateJwkThumbprint({ jwk: toJwk(args.secp256k1ManagedKeyInfo.publicKeyHex, 'Secp256k1') }),
+        isSecp256k1: true,
+        publicKey: formatEbsiPublicKey({ key: args.secp256k1ManagedKeyInfo, type: 'Secp256k1' }),
+        notBefore: args.notBefore,
+        notAfter: args.notAfter,
+      },
+    ],
+    id: args.id,
+    apiOpts: args.apiOpts,
+    token: '', //TODO hook it up: https://sphereon.atlassian.net/browse/SDK-10
+  })
+
+  await sendTransaction(
+    { docTransactionResponse: insertDidDocTransaction, kid: args.secp256k1ManagedKeyInfo.kid, id: args.id, apiOpts: args.apiOpts },
+    context
+  )
+
+  const addVerificationMethodTransaction = await addVerificationMethod({
+    params: [
+      {
+        from: args.from,
+        did: args.identifier.did,
+        isSecp256k1: true,
+        vMethoddId: await calculateJwkThumbprint({ jwk: toJwk(args.secp256k1ManagedKeyInfo.publicKeyHex, 'Secp256k1') }),
+        publicKey: formatEbsiPublicKey({ key: args.secp256k1ManagedKeyInfo, type: 'Secp256k1' }),
+      },
+    ],
+    id: args.id,
+    apiOpts: args.apiOpts,
+    token: '', //TODO hook it up: https://sphereon.atlassian.net/browse/SDK-10
+  })
+
+  await sendTransaction(
+    { docTransactionResponse: addVerificationMethodTransaction, kid: args.secp256k1ManagedKeyInfo.kid, id: args.id, apiOpts: args.apiOpts },
+    context
+  )
+
+  const addVerificationMethodRelationshipTransaction = await addVerificationMethodRelationship({
+    params: [
+      {
+        from: args?.from,
+        did: args.identifier.did,
+        vMethoddId: await calculateJwkThumbprint({ jwk: toJwk(args.secp256r1ManagedKeyInfo.publicKeyHex, 'Secp256r1') }),
+        name: 'assertionMethod',
+        notAfter: 1,
+        notBefore: 1,
+      },
+    ],
+    id: args.id,
+    apiOpts: args.apiOpts,
+    token: '', //TODO hook it up: https://sphereon.atlassian.net/browse/SDK-10
+  })
+
+  await sendTransaction(
+    {
+      docTransactionResponse: addVerificationMethodRelationshipTransaction,
+      kid: args.secp256k1ManagedKeyInfo.kid,
+      id: args.id,
+      apiOpts: args.apiOpts,
+    },
+    context
+  )
 }
