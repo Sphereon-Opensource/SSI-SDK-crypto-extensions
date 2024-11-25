d7b63a89fa54a243749a796d456d5a3fb5c4ffb9
John Melati <github@jcmelati.com>
2024-11-25 14:32:38 +0100

    chore: remove logs

--- packages/kms-azure/src/__tests__/azure-keyvault-key-management-system.test.ts
+++ packages/kms-azure/src/__tests__/azure-keyvault-key-management-system.test.ts
@@ -10,11 +10,6 @@ describe('Key creation', () => {
     const keyVaultClientId = process.env.AZURE_KEYVAULT_CLIENT_ID
     const keyVaultClientSecret = process.env.AZURE_KEYVAULT_CLIENT_SECRET
 
-    console.log("keyVaultUrl env var found: " + keyVaultUrl)
-    console.log("keyVaultClientIdTenantId env var found: " + keyVaultClientIdTenantId)
-    console.log("keyVaultClientId env var found: " + keyVaultClientId)
-    console.log("keyVaultClientSecret env var found: " + typeof(keyVaultClientSecret))
-
     if (!keyVaultUrl || !keyVaultClientIdTenantId || !keyVaultClientId || !keyVaultClientSecret) {
         throw new Error("Missing Azure KeyVault test environment variables")
     }
