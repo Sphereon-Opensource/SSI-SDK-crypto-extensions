eefd2b3ad840aa9e87646b33f157878045df90b9
Niels Klomp <nklomp@sphereon.com>
2024-12-01 05:26:28 +0100

    chore: improve validateJwk function for non kid generation use cases

--- packages/key-utils/src/jwk-jcs.ts
+++ packages/key-utils/src/jwk-jcs.ts
@@ -1,5 +1,5 @@
-import type { ByteView } from 'multiformats/codecs/interface'
-import { TextDecoder, TextEncoder } from 'web-encoding'
+import type {ByteView} from 'multiformats/codecs/interface'
+import {TextDecoder, TextEncoder} from 'web-encoding'
 
 const textEncoder = new TextEncoder()
 const textDecoder = new TextDecoder()
@@ -9,11 +9,15 @@ const textDecoder = new TextDecoder()
  *
  * @param value - The value to check.
  * @param description - Description of the value to check.
+ * @param optional
  */
-function check(value: unknown, description: string) {
-  if (typeof value !== 'string' || !value) {
-    throw new Error(`${description} missing or invalid`)
-  }
+function check(value: unknown, description: string, optional: boolean = false) {
+    if (optional && !value) {
+        return
+    }
+    if (typeof value !== 'string' || !value) {
+        throw new Error(`${description} missing or invalid`)
+    }
 }
 
 /**
@@ -22,9 +26,9 @@ function check(value: unknown, description: string) {
  * @param value - The value to check.
  */
 function assertObject(value: unknown) {
-  if (!value || typeof value !== 'object') {
-    throw new Error('Value must be an object')
-  }
+    if (!value || typeof value !== 'object') {
+        throw new Error('Value must be an object')
+    }
 }
 
 /**
@@ -34,36 +38,40 @@ function assertObject(value: unknown) {
  * @see https://www.rfc-editor.org/rfc/rfc8037#section-2
  *
  * @param jwk - The JWK to check.
+ * @param opts
  */
-export function validateJwk(jwk: any) {
-  assertObject(jwk)
-  // Check JWK required members based on the key type
-  switch (jwk.kty) {
-    /**
-     * @see https://www.rfc-editor.org/rfc/rfc7518#section-6.2.1
-     */
-    case 'EC':
-      check(jwk.crv, '"crv" (Curve) Parameter')
-      check(jwk.x, '"x" (X Coordinate) Parameter')
-      check(jwk.y, '"y" (Y Coordinate) Parameter')
-      break
-    /**
-     * @see https://www.rfc-editor.org/rfc/rfc8037#section-2
-     */
-    case 'OKP':
-      check(jwk.crv, '"crv" (Subtype of Key Pair) Parameter')
-      check(jwk.x, '"x" (Public Key) Parameter')
-      break
-    /**
-     * @see https://www.rfc-editor.org/rfc/rfc7518#section-6.3.1
-     */
-    case 'RSA':
-      check(jwk.e, '"e" (Exponent) Parameter')
-      check(jwk.n, '"n" (Modulus) Parameter')
-      break
-    default:
-      throw new Error('"kty" (Key Type) Parameter missing or unsupported')
-  }
+export function validateJwk(jwk: any, opts?: { crvOptional?: boolean }) {
+    assertObject(jwk)
+    const {crvOptional = false} = opts ?? {}
+    check(jwk.kty, '"kty" (Key Type) Parameter', false)
+
+    // Check JWK required members based on the key type
+    switch (jwk.kty) {
+        /**
+         * @see https://www.rfc-editor.org/rfc/rfc7518#section-6.2.1
+         */
+        case 'EC':
+            check(jwk.crv, '"crv" (Curve) Parameter', crvOptional)
+            check(jwk.x, '"x" (X Coordinate) Parameter')
+            check(jwk.y, '"y" (Y Coordinate) Parameter')
+            break
+        /**
+         * @see https://www.rfc-editor.org/rfc/rfc8037#section-2
+         */
+        case 'OKP':
+            check(jwk.crv, '"crv" (Subtype of Key Pair) Parameter', crvOptional) // Shouldn't this one always be true as crv is not always present?
+            check(jwk.x, '"x" (Public Key) Parameter')
+            break
+        /**
+         * @see https://www.rfc-editor.org/rfc/rfc7518#section-6.3.1
+         */
+        case 'RSA':
+            check(jwk.e, '"e" (Exponent) Parameter')
+            check(jwk.n, '"n" (Modulus) Parameter')
+            break
+        default:
+            throw new Error('"kty" (Key Type) Parameter missing or unsupported')
+    }
 }
 
 /**
@@ -73,17 +81,17 @@ export function validateJwk(jwk: any) {
  * @returns The JWK with only the required members, ordered lexicographically.
  */
 export function minimalJwk(jwk: any) {
-  // "default" case is not needed
-  // eslint-disable-next-line default-case
-  switch (jwk.kty) {
-    case 'EC':
-      return { crv: jwk.crv, kty: jwk.kty, x: jwk.x, y: jwk.y }
-    case 'OKP':
-      return { crv: jwk.crv, kty: jwk.kty, x: jwk.x }
-    case 'RSA':
-      return { e: jwk.e, kty: jwk.kty, n: jwk.n }
-  }
-  throw Error(`Unsupported key type (kty) provided: ${jwk.kty}`)
+    // "default" case is not needed
+    // eslint-disable-next-line default-case
+    switch (jwk.kty) {
+        case 'EC':
+            return {crv: jwk.crv, kty: jwk.kty, x: jwk.x, y: jwk.y}
+        case 'OKP':
+            return {crv: jwk.crv, kty: jwk.kty, x: jwk.x}
+        case 'RSA':
+            return {e: jwk.e, kty: jwk.kty, n: jwk.n}
+    }
+    throw Error(`Unsupported key type (kty) provided: ${jwk.kty}`)
 }
 
 /**
@@ -97,9 +105,9 @@ export function minimalJwk(jwk: any) {
  * @returns Uint8Array-encoded JWK.
  */
 export function jwkJcsEncode(jwk: unknown): Uint8Array {
-  validateJwk(jwk)
-  const strippedJwk = minimalJwk(jwk)
-  return textEncoder.encode(jcsCanonicalize(strippedJwk))
+    validateJwk(jwk)
+    const strippedJwk = minimalJwk(jwk)
+    return textEncoder.encode(jcsCanonicalize(strippedJwk))
 }
 
 /**
@@ -109,67 +117,67 @@ export function jwkJcsEncode(jwk: unknown): Uint8Array {
  * @returns The corresponding JSON Web Key.
  */
 export function jwkJcsDecode(bytes: ByteView<JsonWebKey>): JsonWebKey {
-  const jwk = JSON.parse(textDecoder.decode(bytes))
-  validateJwk(jwk)
-  if (JSON.stringify(jwk) !== jcsCanonicalize(minimalJwk(jwk))) {
-    throw new Error('The JWK embedded in the DID is not correctly formatted')
-  }
-  return jwk
+    const jwk = JSON.parse(textDecoder.decode(bytes))
+    validateJwk(jwk)
+    if (JSON.stringify(jwk) !== jcsCanonicalize(minimalJwk(jwk))) {
+        throw new Error('The JWK embedded in the DID is not correctly formatted')
+    }
+    return jwk
 }
 
 // From: https://github.com/cyberphone/json-canonicalization
 export function jcsCanonicalize(object: any) {
-  let buffer = ''
-  serialize(object)
-  return buffer
+    let buffer = ''
+    serialize(object)
+    return buffer
 
-  function serialize(object: any) {
-    if (object === null || typeof object !== 'object' || object.toJSON != null) {
-      /////////////////////////////////////////////////
-      // Primitive type or toJSON - Use ES6/JSON     //
-      /////////////////////////////////////////////////
-      buffer += JSON.stringify(object)
-    } else if (Array.isArray(object)) {
-      /////////////////////////////////////////////////
-      // Array - Maintain element order              //
-      /////////////////////////////////////////////////
-      buffer += '['
-      let next = false
-      object.forEach((element) => {
-        if (next) {
-          buffer += ','
+    function serialize(object: any) {
+        if (object === null || typeof object !== 'object' || object.toJSON != null) {
+            /////////////////////////////////////////////////
+            // Primitive type or toJSON - Use ES6/JSON     //
+            /////////////////////////////////////////////////
+            buffer += JSON.stringify(object)
+        } else if (Array.isArray(object)) {
+            /////////////////////////////////////////////////
+            // Array - Maintain element order              //
+            /////////////////////////////////////////////////
+            buffer += '['
+            let next = false
+            object.forEach((element) => {
+                if (next) {
+                    buffer += ','
+                }
+                next = true
+                /////////////////////////////////////////
+                // Array element - Recursive expansion //
+                /////////////////////////////////////////
+                serialize(element)
+            })
+            buffer += ']'
+        } else {
+            /////////////////////////////////////////////////
+            // Object - Sort properties before serializing //
+            /////////////////////////////////////////////////
+            buffer += '{'
+            let next = false
+            Object.keys(object)
+                .sort()
+                .forEach((property) => {
+                    if (next) {
+                        buffer += ','
+                    }
+                    next = true
+                    ///////////////////////////////////////////////
+                    // Property names are strings - Use ES6/JSON //
+                    ///////////////////////////////////////////////
+                    buffer += JSON.stringify(property)
+                    buffer += ':'
+                    //////////////////////////////////////////
+                    // Property value - Recursive expansion //
+                    //////////////////////////////////////////
+                    serialize(object[property])
+                })
+            buffer += '}'
         }
-        next = true
-        /////////////////////////////////////////
-        // Array element - Recursive expansion //
-        /////////////////////////////////////////
-        serialize(element)
-      })
-      buffer += ']'
-    } else {
-      /////////////////////////////////////////////////
-      // Object - Sort properties before serializing //
-      /////////////////////////////////////////////////
-      buffer += '{'
-      let next = false
-      Object.keys(object)
-        .sort()
-        .forEach((property) => {
-          if (next) {
-            buffer += ','
-          }
-          next = true
-          ///////////////////////////////////////////////
-          // Property names are strings - Use ES6/JSON //
-          ///////////////////////////////////////////////
-          buffer += JSON.stringify(property)
-          buffer += ':'
-          //////////////////////////////////////////
-          // Property value - Recursive expansion //
-          //////////////////////////////////////////
-          serialize(object[property])
-        })
-      buffer += '}'
     }
-  }
 }
